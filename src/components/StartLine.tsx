0ebac2b911ffef5c220b90609099f57fb6db3d97
arsomargaryan <arsen.margaryan1@mail.ru>
2024-04-22 04:59:37 +0400

    :style: fix stard end lines

--- src/components/StartLine.tsx
+++ src/components/StartLine.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function StartLine() {
   return (
-    <div className="-z-20 absolute text-red-500 border border-gray-400 bg-gray-100 w-7 h-full left-[14%] top-0 flex justify-center items-center">
+    <div className="-z-20 absolute text-red-500 border border-gray-400 bg-gray-100 w-7 h-full left-[147px] top-0 flex justify-center items-center">
       <div className="rotate-90 text-2xl">Start</div>
     </div>
   );
